1c612e8919b16b2feb3a76c7bfaa5b1733bff06a
tanya <vitkouskaya@gmail.com>
2017-08-14 21:59:37 -0700

    Add functionality to 'exit' button

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-var newListElement = '<li><a href="" id="close_box">x</a><input type="checkbox" name="task_name" class="strikethrough" value="1"><span id="task_name">%data%</span></li>';
+var newListElement = '<li><a onclick="return remove_item(this)" href="" id="close_box%item_id%">x</a><input type="checkbox" name="task_name" class="strikethrough" value="1"><span id="task_name">%data%</span></li>';
+
+
+var nextId=1;
 
 function add_item() {
 	var showText = document.getElementById("custom_textbox");
-	var newListElement_edit = newListElement.replace("%data%", showText.value);
-	alert(showText.value);
+	var newListElement_edit = newListElement.replace("%data%", showText.value).replace('%item_id%', nextId);
+	nextId+=1;
 	$("#list").append(newListElement_edit);
 	document.getElementById('custom_textbox').value='';
 
+}
+
+function remove_item(close_a) {
+	
+	$(close_a).parent('li').remove();
+	return false;
 }
