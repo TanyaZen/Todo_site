45a226003cfab41b884cc4f6fc14dba7e7efab8e
tanya <vitkouskaya@gmail.com>
2017-08-16 12:41:34 -0700

    Center paragraph in first loading page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 var newListElement = '<li><a onclick="return remove_item(this)" href="" class="close_box" id="close_box%item_id%">x</a><input type="checkbox" name="task_name" class="strikethrough" value="1"><span id="task_name">%data%</span><span id="task_time">%current_time%</span></li>';
 
 
+function setFocusToTextBox(){
+    document.getElementById("custom_textbox").focus();
+};
+
 var nextId=1;
+
 
 function add_item() {
 	var showText = document.getElementById("custom_textbox");
@@ -9,8 +14,10 @@ function add_item() {
 	nextId+=1;
 	$("#list").append(newListElement_edit);
 	$('p').hide();
+	$(".paragraph").css('height', '20px');    	
 	document.getElementById('custom_textbox').value='';
-
+	setFocusToTextBox();
+    	
 }
 
 function remove_item(close_a) {
@@ -27,7 +34,18 @@ function time_getter() {
 	var d = n.getDate();
 	var e = n.getHours();
 	var f = n.getMinutes();
+	if (e < 10) {e = "0"+e;}
+	if (f < 10) {f = "0"+f;}
 
 	var vremya = m + "/" + d + "/" + y + " " + e + ":" + f;
 	return vremya;
 }
+
+$( document ).ready(function() {
+	$("#custom_textbox").keyup(function(event){
+    	if(event.keyCode == 13){
+        	$("#but").click();
+    	}
+    })
+});
+
