20a4adee3b09b1940f64740a9cd3386bcff0b8d7
tanya <vitkouskaya@gmail.com>
2017-08-16 16:50:38 -0700

    Create array of task objects

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-var newListElement = '<li><a onclick="return remove_item(this)" href="" class="close_box" id="close_box%item_id%">x</a><input type="checkbox" name="task_name" class="strikethrough" value="1"><span id="task_name">%data%</span><span id="task_time">%current_time%</span></li>';
+function taskObj (task_id, task_name, date_time) {
+				this.task_id = task_id;
+            	this.task_name = task_name;
+            	this.date_time = date_time;
+            }; 
+
+var task_array = [];
+
+var newListElement = '<li><a onclick="return remove_item(this)" href="" class="close_box" id="close_box%item_id%">x</a><input type="checkbox" name="task_name" class="strikethrough"><span id="task_name%item_id%">%data%</span><span id="task_time%item_id%">%current_time%</span></li>';
 
 
 function setFocusToTextBox(){
@@ -6,15 +14,15 @@ function setFocusToTextBox(){
 };
 
 var nextId=1;
-
-
 function add_item() {
 	var showText = document.getElementById("custom_textbox");
-	var newListElement_edit = newListElement.replace("%data%", showText.value).replace('%item_id%', nextId).replace("%current_time%", time_getter());
+	var newListElement_edit = newListElement.replace("%data%", showText.value).replace(/%item_id%/g, nextId).replace("%current_time%", time_getter());
+	var new_task = new taskObj('task'+this.nextId, showText.value, time_getter());
+	task_array.push(new_task); 
 	nextId+=1;
 	$("#list").append(newListElement_edit);
 	$('p').hide();
-	$(".paragraph").css('height', '20px');    	
+	$(".paragraph").css('height', '20px');   	
 	document.getElementById('custom_textbox').value='';
 	setFocusToTextBox();
     	
